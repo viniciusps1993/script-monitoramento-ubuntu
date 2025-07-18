0259f25a7a7e08dca45e84a244a266e1528f3e69
viniciusps1993 <vpsndv@gmail.com>
2025-07-17 21:44:31 -0300

    Upload

---
+++ monitoramento-sistema.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOG_DIR="monitoramento_sistema"
+mkdir -p $LOG_DIR
+
+function monitorar_logs() {
+        grep -E "fail(ed)?|error|denied|unauthorized" /var/log/syslog | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoramento_logs_sistema.txt
+        grep -E "fail(ed)?|error|denied|unauthorized" /var/log/auth.log | awk '{print $1, $2, $3, $5, $6, $7}' > $LOG_DIR/monitoramento_logs_auth.txt
+}
+
+function monitorar_rede() {
+        if ping -c 1 8.8.8.8 > /dev/null; then
+                echo "$(date): Conectividade ativa." >> $LOG_DIR/monitoramento_rede.txt
+        else
+                echo "$(date): Sem conexao com a internet." >> $LOG_DIR/monitoramento_rede.txt
+        fi
+
+        if curl -s --head https://www.youtube.com/ | grep "HTTP/2 200" > /dev/null; then
+                echo "$(date): Conexao com o YouTube bem-sucedida." >> $LOG_DIR/monitoramento_rede.txt
+        else
+                echo "$(date): Falha ao conectar com o YouTube." >> $LOG_DIR/monitoramento_rede.txt
+        fi
+}
+
+function executar_monitoramento() {
+        monitorar_logs
+        monitorar_rede
+}
+
+executar_monitoramento
