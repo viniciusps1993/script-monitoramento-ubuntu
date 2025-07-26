abec6f88e2a9e9f0e91e35a2cbb40219a72532cc
viniciusps1993 <vpsndv@gmail.com>
2025-07-25 21:08:59 -0300

    Subindo script de monitoramento para o GitHub

--- monitoramento-sistema.sh
+++ monitoramento-sistema.sh
@@ -22,9 +22,18 @@ function monitorar_rede() {
         fi
 }
 
+function monitorar_disco() {
+    echo "$(date)" >> $LOG_DIR/monitoramento_disco.txt
+    df -h | grep -v "snapfuse" | awk '$5+0 > 70 {print $1 " esta com " $5 " de uso."}' >> $LOG_DIR/monitoramento_disco.txt
+    echo "Uso de disco no diretório principal:" >> $LOG_DIR/monitoramento_disco.txt
+    du -sh /home/vinicius_souza >> $LOG_DIR/monitoramento_disco.txt
+}
+
+
 function executar_monitoramento() {
         monitorar_logs
         monitorar_rede
+        monitorar_disco
 }
 
 executar_monitoramento
